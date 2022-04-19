1262d05eec8ca85bde7de95986e0e80da742f281
pdaup <p.dauparas@gmail.com>
2022-04-19 20:04:38 +0300

    update

--- models.py
+++ models.py
@@ -1,42 +1,50 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
+from sqlalchemy import Column, String, Integer, ForeignKey, Table, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 engine = create_engine("sqlite:///db.db")
 Base = declarative_base()
 
+game_players = Table('Game_players', Base.metadata,
+    Column('game_id', Integer, ForeignKey('game.id')),
+    Column('player_id', Integer, ForeignKey('player.id'))
+)
+
 class Player(Base):
     __tablename__ = "player"
     id = Column(Integer, primary_key=True, autoincrement=True)
     fname = Column("First_name", String)
-    lname = Column("Last_name", String)
-    age = Column("Age", Integer)
+    lname = Column("Last_name", String, nullable=True)
+    age = Column("Age", Integer, nullable=True)
+    games = relationship('Game', secondary=game_players, back_populates='players')
+
+    def __init__(self, fname, lname, age):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
 
     def __repr__(self):
-        return
+        return f"{self.fname}"
 
 class Game(Base):
     __tablename__ = "game"
     id = Column(Integer, primary_key=True, autoincrement=True)
     date = Column("Date", String)
-    player_id = Column("Winner_ID", Integer, ForeignKey('player.id'), nullable=True)
-    result_id = Column("Winner_score_ID", Integer, ForeignKey('result.id'), nullable=True)
-    player = relationship('Player')
-    result = relationship('Result')
+    players = relationship('Player', secondary=game_players, back_populates='games')
+    results = relationship('Result', back_populates='game')
 
     def __repr__(self):
-        return
+        return f"{self.id}. {self.date}"
 
 class Result(Base):
     __tablename__ = "result"
-    id = Column(Integer, primary_key=True)
-    game_id = Column("Game_ID", Integer, ForeignKey('game.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    game_id = Column("Game_id", Integer, ForeignKey('game.id'))
     player_id = Column("Player_ID", Integer, ForeignKey('player.id'))
     score = Column("Score", Integer)
-    game = relationship('Game')
-    player = relationship('Player')
+    game = relationship('Game', back_populates='results')
 
     def __repr__(self):
-        return
+        return f"{self.id}. game {self.game_id}, player {self.player_id}, score {self.score}"
 
 Base.metadata.create_all(engine)
--- programa.py
+++ programa.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import *
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk, Image
 
 engine = create_engine("sqlite:///db.db")
@@ -10,6 +11,8 @@ session = sessionmaker(bind=engine)()
 class root:
     def __init__(self, master):
         self.master = master
+        self.master['padx'] = 5
+        self.master['pady'] = 5
         self.banner_img = ImageTk.PhotoImage(Image.open("banner.jpg"))
         self.banner = Label(master, image=self.banner_img)
         self.banner.grid(row=0, columnspan=3)
@@ -22,25 +25,105 @@ class root:
         self.exit_button = Button(master, text="Exit", command=self.kill)
         self.exit_button.grid(row=4, column=1)
 
-    def new_game(self, master):
+    def new_game(self):
         self.new = Toplevel(self.master)
-        self.app = New_Game_Window(self.new)
+        self.app = Pre_Game_Window(self.new)
 
-    def load_game(self, master):
+    def load_game(self):
         self.new = Toplevel(self.master)
         self.app = Load_Window(self.new)
 
-    def see_history(self, master):
+    def see_history(self):
         self.new = Toplevel(self.master)
         self.app = History_Window(self.new)
 
     def kill(self):
         self.master.destroy()
 
-class New_Game_Window:
-    def __init__(self):
+class Pre_Game_Window:
+    def __init__(self, master):
+        self.master = master
+        self.master['padx'] = 5
+        self.master['pady'] = 5
+        self.playerbase_label = Label(master, text="Choose players")
+        self.playerbase_label.grid(row=0, column=1)
+        self.playerbase_listbox = Listbox(master)
+        self.playerbase_listbox.grid(row=1, column=0)
+        self.create_btn = Button(master, text="New player", command=self.new_player)
+        self.create_btn.grid(row=2, column=0)
+        self.chosenplayers_listbox = Listbox(master)
+        self.chosenplayers_listbox.grid(row=1, column=2)
+        self.start_btn = Button(master, text="Start game", command=self.start_game)
+        self.start_btn.grid(row=2, column=1)
+        self.add_btn = Button(master, text="Add", command=self.add_player)
+        self.add_btn.grid(row=1, column=1)
+        self.remove_btn = Button(master, text="Remove", command=self.remove_player)
+        self.remove_btn.grid(row=2, column=2)
+        self.refresh_btn = Button(master, text="⟲", width=2, command=self.load_playerbase)
+        self.refresh_btn.grid(row=0, column=0, sticky=W)
+        self.load_playerbase()
+
+    def load_playerbase(self):
+        self.playerbase_listbox.delete(0, self.playerbase_listbox.size())
+        list = session.query(Player).all()
+        [self.playerbase_listbox.insert(END, i) for i in list]
+
+    def new_player(self):
+        self.new = Toplevel(self.master)
+        self.app = New_Player_Window(self.new)
+
+    def add_player(self):
+        self.chosenplayers_listbox.insert(END, self.playerbase_listbox.get(ANCHOR))
+
+    def remove_player(self):
+        self.chosenplayers_listbox.delete(self.chosenplayers_listbox.curselection())
+
+    def start_game(self):
         pass
 
+class New_Player_Window:
+    def __init__(self, master):
+        self.master = master
+        self.fname_label = Label(master, text="First name")
+        self.fname_label.grid(row=0, column=0)
+        self.fname_entry = Entry(master)
+        self.fname_entry.grid(row=0, column=1)
+        self.fname_entry.bind("<Return>", lambda event: self.create_new())
+        self.lname_label = Label(master, text="Last name")
+        self.lname_label.grid(row=1, column=0)
+        self.lname_entry = Entry(master)
+        self.lname_entry.grid(row=1, column=1)
+        self.lname_entry.bind("<Return>", lambda event: self.create_new())
+        self.age_label = Label(master, text="Age")
+        self.age_label.grid(row=2, column=0)
+        self.age_entry = Entry(master)
+        self.age_entry.grid(row=2, column=1)
+        self.age_entry.bind("<Return>", lambda event: self.create_new())
+        self.submit_btn = Button(master, text="Submit", command=self.create_new)
+        self.submit_btn.grid(row=3, column=1)
+
+    def create_new(self):
+        fname = self.fname_entry.get()
+        lname = self.lname_entry.get()
+        age = self.age_entry.get()
+        player = Player(fname, lname, age)
+        session.add(player)
+        session.commit()
+        self.master.destroy()
+        messagebox.showinfo('Success', 'Player created successfully!')
+    
+class Game_Window:
+    def __init__(self, master):
+        self.master = master
+        self.playernametitle_lbl = Label(master, text="Player name")
+        self.playernametitle_lbl.grid(row=0, column=0)
+        for i in range(1,11):
+            label = Label(master, text=f"{i}")
+            label.grid(row=0, column=f"{i}")
+        self.sum_lbl = Label(master, text="Total")
+        self.sum_lbl.grid(row=0, column=11)
+        self.add_player_btn = Button(master, text="Add player")
+    
 class Load_Window:
     def __init__(self):
         pass
